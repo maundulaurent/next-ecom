d26858be967d4e1ae14206fb5dc037f5e624126b
maundulaurent <charlesmaundu16@gmail.com>
2025-03-30 22:52:21 +0300

    initialized all pages

---
+++ src/app/product-details/page.tsx
@@ -0,0 +1,8 @@
+
+export default function ProductDetails() {
+    return (
+      <div>
+        <h1>Hello Products Page</h1>
+      </div>
+    )
+  }
---
+++ src/app/shop.tsx/page.tsx
@@ -0,0 +1,8 @@
+
+export default function ShopPage() {
+    return (
+      <div>
+        <h1>Hello Shop Page</h1>
+      </div>
+    )
+  }
